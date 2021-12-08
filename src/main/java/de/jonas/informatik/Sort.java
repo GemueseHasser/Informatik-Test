cd072622de51de58f78eb9f7e7eb23d638f82567
Jonas Lobe <jonas.lobe.2006@outlook.com>
2021-12-08 21:59:40 +0100

    add missing Javadoc

--- src/main/java/de/jonas/informatik/Sort.java
+++ src/main/java/de/jonas/informatik/Sort.java
@@ -1,25 +1,56 @@
 package de.jonas.informatik;
 
+/**
+ * Mit einem {@link Sort} lässt sich ein Array aus Zahlen auf verschiedene Arten bearbeiten und sortieren.
+ */
 public final class Sort {
 
+    //<editor-fold desc="CONSTANTS">
+    /** Das Array aus Zahlen, welche sortiert werden sollen. */
     private static final int[] NUMBERS = {9, 5, 6, 7, 3, 1};
+    //</editor-fold>
 
 
+    //<editor-fold desc="LOCAL FIELDS">
+    /** Das Array aus Zahlen, mit dem die Instanz dieses {@link Sort} arbeitet. */
     private final int[] numbers;
+    //</editor-fold>
 
-    // main method
-    public static void main(String[] args) {
+
+    //<editor-fold desc="main">
+
+    /**
+     * Die Main-Methode dieser Klasse. Diese wird als aller erstes von der JRE aufgerufen und mithilfe dieser Methode
+     * wird zu Test-Zwecken eine Abfolge an Zahlen sortiert.
+     *
+     * @param args Die Argumente, die von der JRE übergeben werden.
+     */
+    public static void main(final String[] args) {
         Sort sort = new Sort(NUMBERS);
         sort.show();
         sort.selection_sort();
         System.out.print("\n\n");
     }
+    //</editor-fold>
 
+
+    //<editor-fold desc="CONSTRUCTORS">
+
+    /**
+     * Erzeugt eine neue und vollständig unabhängige Instanz eines {@link Sort}. Mit einem {@link Sort} lässt sich ein
+     * Array aus Zahlen auf verschiedene Arten bearbeiten und sortieren.
+     *
+     * @param numbers Das Array, bestehend aus Zahlen, welches mithilfe dieses {@link Sort} editiert bzw. sortiert
+     *                werden soll.
+     */
     public Sort(final int[] numbers) {
         this.numbers = numbers;
     }
+    //</editor-fold>
 
-    // print all numbers in a row
+    /**
+     * Zeigt den aktuellen Stand der {@code numbers} an, bzw. gibt die aktuellen {@code numbers} aus.
+     */
     public void show() {
         System.out.print("\n  ");
 
@@ -28,7 +59,11 @@ public final class Sort {
         }
     }
 
-    // sort the numbers, beginning with the smallest, using selection sort
+    /**
+     * Sortiert die {@code numbers} nach dem Prinzip 'Selection Sort'. Bei diesem Prinzip wird die Abfolge an Zahlen
+     * beginnend bei der kleinsten schritt für schritt sortiert. Also zuerst wird die kleinste Zahl herausgesucht und an
+     * die erste Stelle der {@code numbers} gesetzt. Dann wird dasselbe mit der zweit-größten Zahl gemacht, usw.
+     */
     public void selection_sort() {
         for (int i = 0; i < this.numbers.length; i++) {
             // pos_min: position of the smallest number
@@ -70,10 +105,17 @@ public final class Sort {
         }
     }
 
-    // exchange the numbers at positions a and b in the array
-    private void exchange(final int a, final int b) {
-        final int c = this.numbers[a];
-        this.numbers[a] = this.numbers[b];
-        this.numbers[b] = c;
+    /**
+     * Tauscht zwei verschiedene Zahlen innerhalb der {@code numbers} aus. Also die erste übergebene Zahl (die {@code
+     * base}) wird ersetzt durch die zweite übergebene Zahl (die {@code newNumber}) und die erste übergebene Zahl wird
+     * dann an die Stelle der zweiten übergebenen Zahl verschoben.
+     *
+     * @param base      Die Basis, welche durch die {@code newNumber} ausgetauscht wird.
+     * @param newNumber Die neue Zahl, durch die die {@code base} ersetzt wird.
+     */
+    private void exchange(final int base, final int newNumber) {
+        final int c = this.numbers[base];
+        this.numbers[base] = this.numbers[newNumber];
+        this.numbers[newNumber] = c;
     }
 }
