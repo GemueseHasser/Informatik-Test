aa6f82591e155ccc02698dbb4aa778b6b5b9f4e2
Jonas Lobe <jonas.lobe.2006@outlook.com>
2021-12-14 20:34:40 +0100

    calculate frame size individually

--- src/main/java/de/jonas/informatik/Circlet.java
+++ src/main/java/de/jonas/informatik/Circlet.java
@@ -12,17 +12,15 @@ import java.awt.Graphics;
  */
 public final class Circlet extends JFrame {
 
-    //<editor-fold desc="CONSTANTS">
-    /** Die Fenster-Größe, auf dem das {@link Circlet} gezeichnet werden soll (Das Fenster ist Quadratisch). */
-    private static final int FRAME_SIZE = 600;
-    //</editor-fold>
-
-
     //<editor-fold desc="LOCAL FIELDS">
     /** Der insgesamte Radius des {@link Circlet}. */
     private final int radius;
     /** Die Anzahl an Kreisen, aus denen das {@link Circlet} bestehen soll. */
     private final int circleAmount;
+    /** Die Größe eines jeden Kreises, aus denen das {@link Circlet} besteht. */
+    private final int circleSize;
+    /** Die Fenster-Größe, auf dem das {@link Circlet} gezeichnet werden soll (Das Fenster ist Quadratisch). */
+    private final int frameSize;
     //</editor-fold>
 
 
@@ -36,10 +34,10 @@ public final class Circlet extends JFrame {
     public static void main(final String[] args) {
         final Circlet circlet = new Circlet(200, 20);
         circlet.setTitle("Circlet");
-        circlet.setBounds(0, 0, FRAME_SIZE, FRAME_SIZE);
+        circlet.setBounds(0, 0, circlet.getFrameSize(), circlet.getFrameSize());
         circlet.setLocationRelativeTo(null);
         circlet.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        circlet.setResizable(true);
+        circlet.setResizable(false);
         circlet.setVisible(true);
     }
     //</editor-fold>
@@ -53,7 +51,7 @@ public final class Circlet extends JFrame {
      * angeordnet sind. Der Radius des (imaginären) großen Kreises ist beliebig konfigurierbar (wird bei der
      * Instanziierung festgelegt) und die Anzahl an Kreisen, die auf dem (imaginären) großen Kreis liegen ist ebenfalls
      * beliebig konfigurierbar (wird auch bei der Instanziierung festgelegt). Die Größe eines jeden kleinen Kreises wird
-     * mithilfe des Kosinussatzes berechnet.
+     * mithilfe des Kosinussatzes berechnet, woraus sich dann die Größe des Fensters ergibt.
      *
      * @param radius       Der Radius, den dieses {@link Circlet} insgesamt haben soll.
      * @param circleAmount Die Anzahl an Kreisen, aus denen dieses {@link Circlet} insgesamt bestehen soll.
@@ -64,9 +62,28 @@ public final class Circlet extends JFrame {
     ) {
         this.radius = radius;
         this.circleAmount = circleAmount;
+
+        // calculate the size of every circle with the 'cosine law'
+        this.circleSize = (int) Math.sqrt(
+            (radius * radius) + (radius * radius) - ((2 * radius * radius) * Math.cos(2 * Math.PI / circleAmount))
+        );
+
+        // calculate the frame size
+        this.frameSize = (int) (2.5 * (this.radius + (this.circleSize / 2)));
     }
     //</editor-fold>
 
+
+    /**
+     * Stellt den Getter für die Fenster-Größe dar. Die Fenster-Größe ist für jedes Instanziierte {@link Circlet}
+     * individuell, da diese mit den übergebenen Parametern auf jedes {@link Circlet} angepasst wird bzw. extra
+     * kalkuliert wird.
+     *
+     * @return Die Fenster-Größe.
+     */
+    public int getFrameSize() {
+        return this.frameSize;
+    }
 
     //<editor-fold desc="implementation">
     @Override
@@ -75,25 +92,20 @@ public final class Circlet extends JFrame {
 
         // calculate the middle of the frame
         final int middleX = super.getWidth() / 2;
-        final int middleY = super.getHeight() / 2;
-
-        // calculate the size of every circle with the 'cosine law'
-        final int circleSize = (int) Math.sqrt(
-            (radius * radius) + (radius * radius) - ((2 * radius * radius) * Math.cos(2 * Math.PI / circleAmount))
-        );
+        final int middleY = (super.getHeight() / 2) + 11;
 
         // draw all circles
-        for (int i = 0; i < circleAmount; i++) {
+        for (int i = 0; i < this.circleAmount; i++) {
             // calculate the x- and y-coordinate of the current circle
-            final int x = (int) (radius * Math.cos((2 * Math.PI / circleAmount) * i)) + middleX - (circleSize / 2);
-            final int y = (int) (-(radius * Math.sin((2 * Math.PI / circleAmount) * i))) + middleY - (circleSize / 2);
+            final int x = (int) (this.radius * Math.cos((2 * Math.PI / this.circleAmount) * i)) + middleX - (this.circleSize / 2);
+            final int y = (int) (-(this.radius * Math.sin((2 * Math.PI / this.circleAmount) * i))) + middleY - (this.circleSize / 2);
 
             // draw current circle
             g.drawOval(
                 x,
                 y,
-                circleSize,
-                circleSize
+                this.circleSize,
+                this.circleSize
             );
         }
     }
