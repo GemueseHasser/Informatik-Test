d34e83a60af3884ef75d3167b85edaa19d67f6da
Jonas Lobe <jonas.lobe.2006@outlook.com>
2022-01-05 21:51:03 +0100

    add Javadoc

--- src/main/java/de/jonas/informatik/object/List.java
+++ src/main/java/de/jonas/informatik/object/List.java
@@ -2,17 +2,39 @@ package de.jonas.informatik.object;
 
 import java.util.Arrays;
 
+/**
+ * Eine {@link List} stellt eine Liste dar, in der beliebig viele Objekte eines gleichen Objekt-Typen abgespeichert
+ * werden können.
+ *
+ * @param <ObjectType> Der Typ, aus dem alle Objekte bestehen, die sich in der Liste befinden.
+ */
 public class List<ObjectType> {
 
+    //<editor-fold desc="LOCAL FIELDS">
+    /** Alle Objekte, die sich in der Liste befinden (Alle bestehen aus demselben {@link ObjectType}). */
     private Object[] objects;
+    /** Die aktuelle Größe der Liste. */
     private int size;
+    //</editor-fold>
 
 
+    //<editor-fold desc="CONSTRUCTORS">
+
+    /**
+     * Erzeugt eine neue und vollständig unabhängige Instanz einer Liste, in der beliebig viele Objekte eines gleichen
+     * Objekt-Typen abgespeichert werden können.
+     */
     public List() {
         this.objects = new Object[1];
     }
+    //</editor-fold>
 
 
+    /**
+     * Fügt ein bestimmtes Objekt des entsprechenden {@link ObjectType} zu der Liste hinzu.
+     *
+     * @param object Das Objekt, welches hinzugefügt werden soll.
+     */
     public void add(final ObjectType object) {
         if (this.size >= this.objects.length) {
             incrementCapacity();
@@ -23,6 +45,15 @@ public class List<ObjectType> {
         this.size++;
     }
 
+    /**
+     * Gibt ein bestimmtes Objekt des entsprechenden {@link ObjectType} zurück, welches sich an einer bestimmten Stelle
+     * in der Liste befindet.
+     *
+     * @param position Die Position des Objekts, welches abgefragt wird.
+     *
+     * @return Ein bestimmtes Objekt des entsprechenden {@link ObjectType} zurück, welches sich an einer bestimmten
+     *     Stelle in der Liste befindet.
+     */
     @SuppressWarnings("unchecked")
     public ObjectType get(final int position) {
         if (position < 0 || position >= size) {
@@ -34,11 +65,32 @@ public class List<ObjectType> {
         return (ObjectType) objects[position];
     }
 
+    /**
+     * Gibt alle Objekte des entsprechenden {@link ObjectType} zurück, die sich in der Liste befinden (in Form eines
+     * Arrays).
+     *
+     * @return Alle Objekte des entsprechenden {@link ObjectType} zurück, die sich in der Liste befinden (in Form eines
+     *     Arrays).
+     */
     @SuppressWarnings("unchecked")
     public ObjectType[] getAll() {
         return (ObjectType[]) objects;
     }
 
+    /**
+     * Gibt die aktuelle Größe dieser Liste zurück bzw. gibt zurück, wie viele Objekte sich momentan in dieser Liste
+     * befinden.
+     *
+     * @return die aktuelle Größe dieser Liste zurück bzw. gibt zurück, wie viele Objekte sich momentan in dieser Liste
+     *     befinden.
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    /**
+     * Erhöht die momentane Kapazität der Liste um 1.
+     */
     private void incrementCapacity() {
         this.objects = Arrays.copyOf(this.objects, this.objects.length + 1);
     }
