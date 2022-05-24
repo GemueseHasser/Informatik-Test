19e30c18fc3275a1855a9aaa34ded6d7b48db3d3
Jonas Lobe <jonas.lobe.2006@outlook.com>
2022-05-24 22:58:15 +0200

    implement basic frame application

--- Pacman/src/main/java/de/jonas/informatik/Pacman.java
+++ Pacman/src/main/java/de/jonas/informatik/Pacman.java
@@ -1,4 +1,25 @@
 package de.jonas.informatik;
 
+import de.jonas.informatik.graphics.Gui;
+
+/**
+ * Dies ist die Haupt- und Main-Klasse dieser Anwendung bzw. dieses Pacman-Spiels. Das Spiel ähnelt dem herkömmlichen
+ * Pacman, weshalb es Pacman heißt. Man muss sich durch logisches Verschieben von Objekten den Weg zum Ziel erarbeiten.
+ */
 public class Pacman {
+
+    //<editor-fold desc="main">
+
+    /**
+     * Die Main-Methode dieser Anwendung, welche vor allen anderen Klassen und Methoden direkt von der JRE beim Starten
+     * der Anwendung aufgerufen und ausgeführt wird.
+     *
+     * @param args Die Argumente, die beim Starten der Anwendung von der JRE mitgegeben werden.
+     */
+    public static void main(final String[] args) {
+        // create gui
+        new Gui();
+    }
+    //</editor-fold>
+
 }
