2461224253a90461a04005b66394031e46ea5081
Jonas Lobe <jonas.lobe.2006@outlook.com>
2022-07-19 01:08:19 +0200

    implement computer without ai

--- ExtendedTicTacToe/src/main/java/de/jonas/informatik/tictactoe/object/game/Computer.java
+++ ExtendedTicTacToe/src/main/java/de/jonas/informatik/tictactoe/object/game/Computer.java
@@ -1,9 +1,55 @@
 package de.jonas.informatik.tictactoe.object.game;
 
+import de.jonas.informatik.ExtendedTicTacToe;
+import de.jonas.informatik.tictactoe.constant.PlayerType;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Der Computer ersetzt einen zweiten Spieler, sodass der Nutzer dieses Spiels alleine gegen diesen Computer spielen
+ * kann. Der Computer setzt mithilfe von künstlicher Intelligenz völlig automatisiert.
+ */
 public final class Computer {
 
+    /**
+     * Lässt den Computer seinen Zug vollständig automatisiert ausführen. Nach Beendigung dieses Zugs ist der Nutzer
+     * wieder an der Reihe.
+     */
     public void place() {
+        // place random
+        placeRandom();
 
+        // its players turn :)
+        ExtendedTicTacToe.getGameManager().setPlayersTurn(true);
+    }
+
+    /**
+     * Platziert an einer komplett zufälligen Position auf dem Spielfeld.
+     */
+    private void placeRandom() {
+        int row = ThreadLocalRandom.current().nextInt(0, 20);
+        int column = ThreadLocalRandom.current().nextInt(0, 20);
+
+        while (
+            ExtendedTicTacToe.getGameManager().getFields()[row][column].getPlayerType()
+                != PlayerType.EMPTY
+        ) {
+            row = ThreadLocalRandom.current().nextInt(0, 20);
+            column = ThreadLocalRandom.current().nextInt(0, 20);
+        }
+
+        placeAt(row, column);
+    }
+
+    /**
+     * Platziert an einer exakten Stelle auf dem Spielfeld.
+     *
+     * @param row    Die Zeile, in der der Computer platzieren soll.
+     * @param column Die Spalte, in der der Computer platzieren soll.
+     */
+    private void placeAt(final int row, final int column) {
+        ExtendedTicTacToe.getGameManager().getFields()[row][column].setPlayerType(PlayerType.COMPUTER);
+        ExtendedTicTacToe.getGameManager().getFields()[row][column].getButton().setText(PlayerType.COMPUTER.getSymbol());
     }
 
 }
